86e8cab023139ec419521f5d48f834dfe3f7e2d8
Jean-Paul Mosquera Arevalo <jpmosquera1@utpl.edu.ec>
2019-12-22 20:44:08 -0500

    screen menu

--- lib/screens/general.dart
+++ lib/screens/general.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import '../shared/shared.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter/cupertino.dart';
+
 
 class GeneralScreen extends StatelessWidget {
 
@@ -19,7 +21,47 @@ class GeneralScreen extends StatelessWidget {
         ],
       ),
 
-      body: ,
+      body:SafeArea(
+        child: CupertinoButton(
+          child: Container(
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/mindful.jpg"),
+                fit: BoxFit.cover,
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Container(
+              margin: EdgeInsets.fromLTRB(15, 15, 0, 0),
+              child: Text(
+                "Mindfulness",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600),
+              ),
+            ),
+          ),
+          onPressed: () {
+            showCupertinoDialog(
+              context: context,
+              builder: (BuildContext context) => CupertinoAlertDialog(
+                title: const Text('Card is clicked.'),
+                actions: <Widget>[
+                  CupertinoDialogAction(
+                    child: const Text('ok'),
+                    onPressed: () {
+                      Navigator.pop(context, 'ok');
+                    },
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
 
       bottomNavigationBar: AppBottomNav(),
     );
